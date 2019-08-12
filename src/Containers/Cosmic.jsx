f47f1bf33506665f80b7f833a409f73fda06860a
Travis <travisdesir31@gmail.com>
2019-08-12 08:14:04 -0400

    modified

--- src/Containers/Cosmic.jsx
+++ src/Containers/Cosmic.jsx
@@ -18,6 +18,7 @@ class Cosmic extends Component {
         <h1 className='w3-text-white neon1'>Cosmic Cafe</h1>
       </div>
       <br></br>
+      <p>Cosmic Cafe is a space for caregivers to sit, relax, and enjoy a treat or two, while their children play. Choose from a selection of carefully brewed coffees, espressos or teas. Our menu changes from day to day but it typically includes a variety of baked goods, healthy snack options, and some guilty pleasures! We are a nut-free facility, so everything here is free of peanuts and tree nuts.</p>
       <Grid>
         <Grid.Column mobile={16} tablet={8} computer={4}>
         <Card
